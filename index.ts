7c15caaaf192b3866f75a1522ed7987f7e67b5ec
bryanPEREZ1497 <bam.perez@yavirac.edu.ec>
2022-05-11 15:47:35 -0500

    1 file modified

--- index.ts
+++ index.ts
@@ -101,5 +101,32 @@ function binarySearch(
   return -1;
 }
 
-const numbersTest = [0, 0.1, 0.002, 3, 4, 5, 6, 7, 80, 90, 100, 1000];
-console.log(binarySearch(numbersTest, 0.002, 0, numbersTest.length - 1));
+// const numbersTest = [0, 0.1, 0.002, 3, 4, 5, 6, 7, 80, 90, 100, 1000];
+// console.log(binarySearch(numbersTest, 0.002, 0, numbersTest.length - 1));
+
+function bombDetector(numbers: number[], bomb: number): boolean {
+  let numbersInString: string = numbers.toString();
+
+  return numbersInString.includes(bomb.toString());
+}
+// console.log(bombDetector([1, 2, 3, 4, 5, 6], 7));
+// console.log(bombDetector([1, 2, 3, 4, 5, 6], 6));
+
+/**
+ * searchInsert
+ *
+ * Example 1:-------------------------
+ * Input: nums = [1,3,5,6], target = 5
+ * Output: 2
+ * Example 2:-------------------------
+ * Input: nums = [1,3,5,6], target = 2
+ * Output: 1
+ * Example 3:-------------------------
+ * Input: nums = [1,3,5,6], target = 7
+ * Output: 4
+ */
+function searchInsert(nums: number[], target: number): number {
+  let index = binarySearch(nums, target, 0, nums.length - 1);
+  
+  return -1;
+}
